8ca686c9fd3b66e7d8dfc1246b53de362762e261
LawrenceTo <lawrence1755@gmail.com>
2021-03-05 01:08:35 -0500

    Case1 done

--- AERSP458_Project1.m
+++ AERSP458_Project1.m
@@ -1,6 +1,7 @@
 % AERSP 458 Project 1
 %% Case1
 clear, clc
+format longEng
 
 r0 = [-1.7512, 2.0439, -2.6693]; %[LU]
 v0 = [-2.1843, -0.4926, 0.4740]; %[LU/TU]
@@ -12,7 +13,24 @@ h = cross(r0,v0);
 p = norm(h)^2/mu;
 a = -mu*norm(r0)/(norm(r0)*norm(v0)^2-2*mu);
 e = sqrt(1 - p/a);
+alpha = 1/a;
+sigma0 = dot(r0,v0)/sqrt(mu);
 
+[chi,U0,U1,U2] = UniversalEqn(alpha, mu, t1, t0, r0, sigma0);
+
+F =  1 - 1/norm(r0)*U2;
+G = norm(r0)/sqrt(mu)*U1+sigma0/sqrt(mu)*U2;
+r1 = F*r0 + G*v0;
+Ft = -sqrt(mu)/(norm(r1)*norm(r0))*U1;
+Gt = 1 - U2/norm(r1);
+v1 = Ft*r0 + Gt*v0;
+
+energy0 = norm(v0)^2/2 - mu / norm(r0);
+energy1 = norm(v1)^2/2 - mu / norm(r1);
+
+h1 = cross(r1,v1);
+
+disp("Case 1: -------------")
 %Part 1: the orbit type
 if e == 0
     disp("Orbit shape is CIRCLE");
@@ -23,12 +41,19 @@ elseif e > 1
 else
     disp("Orbit shape is ELLIPSE");
 end
-
-
-
+fprintf("Eccentricity e = %f \n", e);
+fprintf("Semi-major Axis a = %f \n", a);
+fprintf("r1 = \n");
+disp(r1');
+fprintf("v1 = \n");
+disp(v1');
+fprintf("Energy at t0 = %f ; Energy at t1 = %f \n", energy0, energy1);
+fprintf("h at t0 = \n");
+disp(h');
+fprintf("h at t1 = \n");
+disp(h1');
 
 %% Case2
-clear,clc
 r0 = [0.6229, 1.3651, -0.0475]; %[LU]
 v0 = [8.4232, -1.8123, 4.2091]; %[LU/TU]
 t0 = 0;
@@ -39,7 +64,24 @@ h = cross(r0,v0);
 p = norm(h)^2/mu;
 a = -mu*norm(r0)/(norm(r0)*norm(v0)^2-2*mu);
 e = sqrt(1 - p/a);
+alpha = 1/a;
+sigma0 = dot(r0,v0)/sqrt(mu);
 
+[chi,U0,U1,U2] = UniversalEqn(alpha, mu, t1, t0, r0, sigma0);
+
+F =  1 - 1/norm(r0)*U2;
+G = norm(r0)/sqrt(mu)*U1+sigma0/sqrt(mu)*U2;
+r1 = F*r0 + G*v0;
+Ft = -sqrt(mu)/(norm(r1)*norm(r0))*U1;
+Gt = 1 - U2/norm(r1);
+v1 = Ft*r0 + Gt*v0;
+
+energy0 = norm(v0)^2/2 - mu / norm(r0);
+energy1 = norm(v1)^2/2 - mu / norm(r1);
+
+h1 = cross(r1,v1);
+
+disp("Case 1: -------------")
 %Part 1: the orbit type
 if e == 0
     disp("Orbit shape is CIRCLE");
@@ -50,4 +92,15 @@ elseif e > 1
 else
     disp("Orbit shape is ELLIPSE");
 end
+fprintf("Eccentricity e = %f \n", e);
+fprintf("Semi-major Axis a = %f \n", a);
+fprintf("r1 = \n");
+disp(r1');
+fprintf("v1 = \n");
+disp(v1');
+fprintf("Energy at t0 = %f ; Energy at t1 = %f \n", energy0, energy1);
+fprintf("h at t0 = \n");
+disp(h');
+fprintf("h at t1 = \n");
+disp(h1');
 
--- UniversalEqn.m
+++ UniversalEqn.m
@@ -1,6 +1,19 @@
-function [outputArg1,outputArg2] = UniversalEqn(inputArg1,inputArg2)
+function [chi,U0,U1,U2] = UniversalEqn(alpha, mu, t, t0, r0, sigma0)
 %UNIVERSALEQN iterativly solve the Universal Time Eqn
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+tol = 1e-6;
+chiOld = alpha*sqrt(mu)*(t-t0); %initial chi value Guess
+[U0,U1,U2] = TopDown(alpha, chiOld); %inital U0, U1, U2
+chiDiff = chiOld;
+
+while chiDiff >= tol
+    fX = chiOld - sigma0 - sigma0*U0 - (1-alpha*r0)*U1-alpha*sqrt(mu)*(t-t0); %f(X_old)
+    dfX = 1+sigma0*alpha*U1 - (1-alpha*r0)*U0; %f'(X_old)
+    chiNew = chiOld - fX/dfX; %Newton-Raphson
+    chiDiff = abs(chiNew-chiOld); % error
+    chiOld = chiNew; %re-intialize chiOld
+    [U0,U1,U2] = TopDown(alpha, chiNew); %calculate U0,U1,U2 with chiNew
+end
+ chi = chiNew;
+   
 end
 
