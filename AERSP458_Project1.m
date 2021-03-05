cd553520ce8b97612328e48261fd485919223f33
LawrenceTo <lawrence1755@gmail.com>
2021-03-04 20:41:57 -0500

    Top down done

--- AERSP458_Project1.m
+++ AERSP458_Project1.m
@@ -1,4 +1,4 @@
 % AERSP 458 Project 1
-[U0,U1,U2] = TopDown(5, pi);
-testing0 = cos(sqrt(5)*pi);
-testing1 = sinh(sqrt(-5)*pi)/sqrt(-5);
+[U0,U1,U2] = TopDown(5, 19);
+testing0 = cos(sqrt(5)*19);
+testing1 = sinh(sqrt(-5)*19)/sqrt(-5);
